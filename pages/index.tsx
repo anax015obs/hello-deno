7bbdfe86e130a6666003d4fbc7d08f9f41a71193
anax015obs <our4000@gmail.com>
2021-08-06 18:37:47 +0900

    ..

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,12 @@
 import React, { useEffect } from "react";
-import Logo from "../components/logo.tsx";
 import useCounter from "../lib/useCounter.ts";
-const ucworks = await import("ucworks-client-desktop-common");
 
 export default function Home() {
-  useEffect(() => {
-    console.log(ucworks);
-  }, []);
   const [count, isSyncing, increase, decrease] = useCounter();
-  const { isOpen, openModal, closeModal, Modal } = ucworks.useModal();
+
   return (
     <div>
-      <button
-        onClick={() => {
-          openModal();
-        }}
-      >
-        click me!
-      </button>
-      {isOpen && (
-        <Modal>
-          <div>haha</div>
-        </Modal>
-      )}
+      <button>click me!</button>
     </div>
   );
 }
